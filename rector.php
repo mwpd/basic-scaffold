6cbe64df91fa82997dad664cf2bdd0cbf0af4aa4
Alain Schlesser <alain.schlesser@gmail.com>
2024-11-15 18:09:35 +0100

    Add Rector config

---
+++ rector.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([SetList::PHP_70]);
